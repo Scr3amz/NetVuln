c05b207710dabc892f4ba006316cbf011ee9cc9f
Scr3amz <mvolchanin@mail.ru>
2024-02-27 22:51:03 +0300

    add logger

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/Scr3amz/NetVuln/internal/logger"
+
+func main() {
+	log := logger.NewLogger("local")
+
+	log.Info("starting application" )
+
+	//TODO: add config
+
+	//TODO: add start of app
+
+}
