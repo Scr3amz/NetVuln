f585f47907cba9268617d8ada483385c25a598fa
Scr3amz <mvolchanin@mail.ru>
2024-02-29 20:06:32 +0300

    change path to generated files

--- protos/gen/vulners_grpc.pb.go
+++ protos/gen/vulners_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v3.12.4
-// source: protos/proto/vulners.proto
+// source: vulners.proto
 
 package vulnv1
 
@@ -101,5 +101,5 @@ var NetVulnService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "protos/proto/vulners.proto",
+	Metadata: "vulners.proto",
 }
