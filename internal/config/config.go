b54e3ae2809b547678e45cdf2f1c221f92bd778c
Scr3amz <mvolchanin@mail.ru>
2024-03-07 00:56:39 +0300

    added config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,56 @@
 package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env        string     `yaml:"env" env-default:"local"`
+	ScriptPath string     `yaml:"scriptPath" env-required:"./vulners.nse"`
+	GRPC       GRPCConfig `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); err != nil {
+		panic("config file does not exist")
+	}
+
+	var config Config
+
+	if err := cleanenv.ReadConfig(path, &config); err != nil {
+		panic("unable to read config file")
+	}
+
+	return &config
+}
+
+// fetchConfigPath fetches config path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var path string
+
+	// --config="./config.yaml"
+	flag.StringVar(&path, "config", "", "config path")
+	flag.Parse()
+
+	if path == "" {
+		path = os.Getenv("CONFIG_PATH")
+	}
+
+	return path
+}
